5ebdee4cab6baee2b82817fc5d5548c093bfcd3b
pranavi13 <78367057+pranavi13@users.noreply.github.com>
2021-02-28 21:20:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var fixedRect, movingRect;
+var edges;
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  edges=createEdgeSprites();
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.bounceOff (edges);
+  fixedRect.bounceOff (edges);
+  bounceoff1(fixedRect, movingRect)
+  
+  drawSprites();
+}
